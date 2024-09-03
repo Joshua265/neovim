05493e8ec44f05e8900a1e5b30d3f56957b7603f
Joshua265 <Joshua_Noel@gmx.de>
2024-09-03 13:42:18 +0200

    initial setup

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "My own Neovim flake";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    neovim = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    telescope-recent-files-src = {
+      url = "github:smartpde/telescope-recent-files";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    neovim,
+    telescope-recent-files-src,
+  }: let
+    overlayFlakeInputs = prev: final: {
+      neovim = neovim.packages.x86_64-linux.neovim;
+
+      vimPlugins =
+        final.vimPlugins
+        // {
+          telescope-recent-files = import ./packages/vimPlugins/telescopeRecentFiles.nix {
+            src = telescope-recent-files-src;
+            pkgs = prev;
+          };
+        };
+    };
+
+    overlayCustomNeovim = prev: final: {
+      customNeovim = import ./packages/customNeovim.nix {
+        pkgs = final;
+      };
+    };
+
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [overlayFlakeInputs overlayCustomNeovim];
+    };
+  in {
+    packages.x86_64-linux.default = pkgs.customNeovim;
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${pkgs.customNeovim}/bin/nvim";
+    };
+  };
+}
---
+++ packages/customNeovim.nix
@@ -0,0 +1,26 @@
+{pkgs}: let
+  customRC = import ../config {inherit pkgs;};
+  plugins = import ../plugins.nix {inherit pkgs;};
+  runtimeDeps = import ../runtimeDeps.nix {inherit pkgs;};
+  neovimRuntimeDependencies = pkgs.symlinkJoin {
+    name = "neovimRuntimeDependencies";
+    paths = runtimeDeps.deps1;
+  };
+  neovimRuntimeDependencies2 = pkgs.symlinkJoin {
+    name = "neovimRuntimeDependencies2";
+    paths = runtimeDeps.deps2;
+  };
+  customNeovimUnwrapped = pkgs.wrapNeovim pkgs.neovim {
+    configure = {
+      inherit customRC;
+      packages.all.start = plugins;
+    };
+  };
+in
+  pkgs.writeShellApplication {
+    name = "nvim";
+    runtimeInputs = [neovimRuntimeDependencies2 neovimRuntimeDependencies];
+    text = ''
+      ${customNeovimUnwrapped}/bin/nvim "$@"
+    '';
+  }
