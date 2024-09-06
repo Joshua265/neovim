b5a28e2fb55324ed595ae3bdbbbda51cfebd25ee
Joshua265 <Joshua_Noel@gmx.de>
2024-09-06 21:07:04 +0200

    remaps

--- config/lua/nvim-telescope.lua
+++ config/lua/nvim-telescope.lua
@@ -2,5 +2,7 @@ local opt = { noremap = true }
 local telescope = require("telescope")
 telescope.setup({})
 telescope.load_extension("recent_files")
-vim.api.nvim_set_keymap("n", "<leader><tab>", ":lua require('telescope.builtin').find_files()<CR>", opt)
+vim.api.nvim_set_keymap("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<CR>", opt)
 vim.api.nvim_set_keymap("n", "<leader><leader>", ":lua require('telescope').extensions.recent_files.pick()<CR>", opt)
+vim.api.nvim_set_keymap("n", "<C-p>", ":lua require('telescope.builtin').git_files()<CR>", opt
+vim.api.nvim_set_keymap("n", "<leader>s", ":lua function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') }) end()<CR>", opt)
